57aa99fd0bc835d6c6228b43d984929ec74a789e
boleknowak <boleknowak@o2.pl>
2023-09-03 16:05:27 +0200

    Updated database

--- prisma/migrations/20230417122619_last_ping_at/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "visits" ADD COLUMN     "last_ping_at" TIMESTAMP(3);
--- prisma/migrations/20230417134012_more_indexes/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "visits_id_token_id_created_at_origin_idx";
-
--- CreateIndex
-CREATE INDEX "visits_id_token_id_created_at_last_ping_at_ended_at_origin_idx" ON "visits"("id", "token_id", "created_at", "last_ping_at", "ended_at", "origin");
